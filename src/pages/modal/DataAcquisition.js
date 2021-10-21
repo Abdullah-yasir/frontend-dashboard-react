a9d3c621de097ad2335ca902ffd186475df60988
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2021-10-22 00:20:13 +0500

    created data acquisition page in data modal

--- src/pages/modal/DataAcquisition.js
+++ src/pages/modal/DataAcquisition.js
@@ -4,11 +4,8 @@ import Button from '../../components/Button';
 
 
 const radioOptions = [
-  { name: 'fb', label: 'Facebook', checked: true },
-  { name: 'gg', label: 'Google' },
-  { name: 'ig', label: 'Instagram' },
-  { name: 'tt', label: 'Twitter' },
-  { name: 'li', label: 'Linkedin' }
+  { name: 'fb', label: 'Allow DataLucent to acquire my data', info: `Our automated data acquisition process will acquire your social media data for you in a secure manner. Upon selecting this option you will be asked to provide your login information for each social media site that you wish for us to acquire your data. Your login information is stored in an encrypted format in a highly secure data repository and is not visible to anyone. If you wish to proceed, please click 'Continue with option 1' button.`, checked: true },
+  { name: 'gg', label: 'I will acquire my data and send it to DataLucent', info: `If you wish to download your data manually go with option 2. You will redirect to new page, where you can see how to download your data from your selected platform manually.` }
 ]
 
 const DataAcquisition = (props) => {
@@ -16,16 +13,17 @@ const DataAcquisition = (props) => {
   return (
     <div>
       <h5 className="modal-subhead">Data Acquisition</h5>
-      <p className="modal-info">Which social media data do you use and want to include as potential social lisence.</p>
-      <p>Select social media to download data.</p>
+      <p className="modal-info">You have TWO options for acquiring your social media data. Please select which option you prefer.</p>
       <div className="media-options">
         <form>
           {radioOptions.map((radio) => {
             return (
-              <div className="radio-btn">
-                <input className="radio-icon" name='option' type="radio" />
-                <label className="radio-label" for={radio.name}>{radio.label}</label>
-              </div>
+              <>
+                <div className="radio-btn">
+                  <input className="radio-icon" name='option' type="radio" />
+                  <label className="radio-label" style={{ fontWeight: 'bold' }} for={radio.name}>{radio.label}</label>
+                </div>
+                <p className="modal-info">{radio.info}</p></>
             );
           })}
         </form>
