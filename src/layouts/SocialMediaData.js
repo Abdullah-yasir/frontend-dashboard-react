54a0f4d94d0efa54fbdd7bbf4a92b344c86f35b8
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2021-10-21 21:09:45 +0500

    adding radio buttons in modal screen 1

--- src/layouts/SocialMediaData.js
+++ src/layouts/SocialMediaData.js
@@ -10,18 +10,34 @@ const numbers = [
 const SocialMediaData = (props) => {
   return (
     <article>
-      <div className="number-tabs">
-        <div className="number-circle active">
-          <span className="number">1</span>
+      <div className="row-center">
+        <h3 className="modal-title-text">Add Social Media</h3>
+      </div>
+      <div className="row-center">
+        <div className="number-tabs">
+          <div className="number-circle active">
+            <span className="number">1</span>
+          </div>
+          <div className="number-bg-line" />
+          <div className="number-circle">
+            <span className="number">2</span>
+          </div>
+          <div className="number-bg-line" />
+          <div className="number-circle">
+            <span className="number">3</span>
+          </div>
         </div>
-        <div className="number-bg-line" />
-        <div className="number-circle">
-          <span className="number">2</span>
-        </div>
-        <div className="number-bg-line" />
-        <div className="number-circle">
-          <span className="number">3</span>
-        </div>
+      </div>
+      <h5 className="modal-subhead">Social Media Data</h5>
+      <p className="modal-info">Which social media data do you use and want to include as potential social lisence.</p>
+      <p>Select social media to download data.</p>
+      <div className="media-options">
+        <form>
+          <div className="radio-btn">
+            <input className="radio-icon" name="facebook" type="radio" />
+            <label className="radio-label" for="facebook">Facebook</label>
+          </div>
+        </form>
       </div>
     </article>
   )
