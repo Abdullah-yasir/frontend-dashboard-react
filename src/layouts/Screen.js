8b38e236477b9c19b3474ec7a47bf9c38059ebad
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2021-10-22 17:14:16 +0500

    fix: overflow in screen layout. screen footer is added

--- src/layouts/Screen.js
+++ src/layouts/Screen.js
@@ -6,6 +6,8 @@ import Footer from './Footer';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
+import logoIcon from '../assets/datalucent-logo-icon.png'
+
 
 const Screen = (props) => {
   const { children } = props;
@@ -19,6 +21,10 @@ const Screen = (props) => {
         <main className="content-right">
           <CornerBg>
             {children}
+            <div className="screen-content-footer">
+              <div className="footer-line" />
+              <img alt="logo-icon" className="footer-icon" src={logoIcon} />
+            </div>
           </CornerBg>
         </main>
       </div>
--- src/pages/dashboard/Offers.js
+++ src/pages/dashboard/Offers.js
@@ -9,6 +9,15 @@ const offers = [
   { brand: 'ABC', offer: `You will be sharing the data from the account you choose.`, image: '' },
   { brand: 'GRD', offer: `You will be sharing the data from the account you choose.`, image: '' },
   { brand: 'GRp', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'ABC', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'GRD', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'GRp', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'ABC', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'GRD', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'GRp', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'ABC', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'GRD', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'GRp', offer: `You will be sharing the data from the account you choose.`, image: '' },
 ]
 
 const OfferCard = (props) => {
