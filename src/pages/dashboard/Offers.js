d3d2612d1f0a70dd91bdb12f60e818edb162c0e7
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2021-10-22 16:51:37 +0500

    basic cards structure in offers

--- src/pages/dashboard/Offers.js
+++ src/pages/dashboard/Offers.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import Button from '../../components/Button';
 
+const offers = [
+  { brand: 'Nike', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'GUCCI', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'KFC', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'XYZ', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'ABC', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'GRD', offer: `You will be sharing the data from the account you choose.`, image: '' },
+  { brand: 'GRp', offer: `You will be sharing the data from the account you choose.`, image: '' },
+]
+
 const OfferCard = (props) => {
   const { brand, offer, brandLogo } = props;
   return (
     <article className="offer-card">
-      <img alt="brand logo" />
-      <p>Brand:<span>{brand}</span></p>
+      <img alt="brand logo" src={brandLogo} />
+      <p>Brand: <span className="offer-card-brand">{brand}</span></p>
       <p>{offer}</p>
-      <div className="buttons-row">
-        <Button label="Accecpt" classes="button-outline-primary" />
-        <Button label="Reject" classes="button-outline-faded" />
+      <div className="buttons-row" style={{ justifyContent: 'flex-end' }}>
+        <Button label="Reject" classes="button-outline-faded group-button" />
+        <Button label="Accecpt" classes="button-outline-primary group-button" />
       </div>
     </article>
   );
@@ -18,9 +28,8 @@ const OfferCard = (props) => {
 
 const Offers = (props) => {
   return (
-    <section>
-      Offers
-      <OfferCard brand="nike" offer="You will be sharing the data from the account you choose." />
+    <section className="container flex-row-wrap">
+      {offers.map((offer) => <OfferCard brand={offer.brand} offer={offer.offer} brandLogo={offer.image} />)}
     </section>
   );
 }
