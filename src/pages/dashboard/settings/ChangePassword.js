0b819b335cf698b5a3cafc4a17b3afd87cb28bc9
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2021-10-26 10:23:29 +0500

    change password structure is complete

--- src/pages/dashboard/settings/ChangePassword.js
+++ src/pages/dashboard/settings/ChangePassword.js
@@ -1,9 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import userAvatarDefault from '../../../assets/user-avatar-default.png';
+import Button from '../../../components/Button';
+import Seperator from '../../../components/Seperator';
+import TextField from '../../../components/TextFields';
 
 const ChangePassword = (props) => {
   return (
     <section className="full width height" >
-      change password
+      <div className="content-center-h">
+        <Seperator vertical />
+        <div className="image-rounded  bg white faded p-4 flex center all">
+          <FontAwesomeIcon icon={['fas', 'lock']} className="big-size color-primary faded" />
+        </div>
+        <Seperator vertical />
+        <div className="width-700">
+          <div className="flex-row-wrap full width">
+            <TextField label="Old Password" type="password" icon="password" />
+          </div>
+          <div className="flex-row-wrap full width">
+            <TextField label="New Password" type="password" icon="password" />
+          </div>
+          <div className="flex-row-wrap full width">
+            <TextField label="Retype New Password" type="password" icon="password" />
+          </div>
+        </div>
+        <Seperator vertical />
+        <Button label="Save Changes" className="button button-outline-primary" />
+      </div>
     </section>
   );
 }
